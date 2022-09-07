8fdf63e7bcbbf2a00faa22e937cbe6563e384b33
cmyui <cmyuiosu@gmail.com>
2022-09-07 01:47:45 -0400

    feat: implement base for service

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ -z "$APP_COMPONENT" ]; then
+  echo "Please set APP_COMPONENT"
+  exit 1
+fi
+
+cd /srv/root
+
+case $APP_COMPONENT in
+  "api")
+    /scripts/run-api.sh
+    ;;
+
+  "tests")
+    /scripts/run-tests.sh
+    ;;
+
+  *)
+    echo "'$APP_COMPONENT' is not a known value for APP_COMPONENT"
+esac
