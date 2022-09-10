adbf4b669ab610c899fe04a5eb8bc3761f00e405
cmyui <cmyuiosu@gmail.com>
2022-09-09 23:47:49 -0400

    fix: adjust accounts models to match core service

--- mount/app/models/accounts.py
+++ mount/app/models/accounts.py
@@ -6,24 +6,25 @@ from uuid import UUID
 from . import BaseModel
 from . import Status
 
+# NOTE: the `country` field is iso-3166-1 alpha-2
 
-class BaseAccount(BaseModel):
+
+class AccountInput(BaseModel):
+    username: str
+    password: str
+    email_address: str
+    country: str
+
+
+class Account(BaseModel):
+    rec_id: int
     account_id: UUID
     username: str
-    safe_username: str
     email_address: str
-    country: str  # iso-3166-1 alpha-2
-
-
-class Account(BaseAccount):
-    rec_id: int
-    status: Status
+    country: str
     created_at: datetime
     updated_at: datetime
-
-
-class AccountInput(BaseAccount):
-    password: str
+    status: Status
 
 
 class AccountUpdate(BaseModel):
