61c25ab207a427c373e766bb0580d260661bf3fa
cmyui <cmyuiosu@gmail.com>
2022-09-11 18:52:21 -0400

    style: rename AccountInput to (more clear) SignupForm

--- mount/app/api/rest/v1/accounts.py
+++ mount/app/api/rest/v1/accounts.py
@@ -1,7 +1,7 @@
 from app.api.rest.context import RequestContext
 from app.api.rest.gateway import forward_request
 from app.models.accounts import Account
-from app.models.accounts import AccountInput
+from app.models.accounts import SignupForm
 from fastapi import APIRouter
 from fastapi import Depends
 
@@ -12,7 +12,7 @@ SERVICE_URL = "http://user-accounts-service"
 
 # https://osuakatsuki.atlassian.net/browse/V2-10
 @router.post("/v1/accounts", response_model=Account)
-async def sign_up(args: AccountInput, ctx: RequestContext = Depends()):
+async def sign_up(args: SignupForm, ctx: RequestContext = Depends()):
     response = await forward_request(ctx,
                                      method="POST",
                                      url=f"{SERVICE_URL}/v1/accounts",
