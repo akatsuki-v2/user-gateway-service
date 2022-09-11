d358807dced6565f512420ec8be1a3b97f44f981
cmyui <cmyuiosu@gmail.com>
2022-09-11 18:52:54 -0400

    feat: add endpoints fetching & updating an account

--- mount/app/api/rest/v1/accounts.py
+++ mount/app/api/rest/v1/accounts.py
@@ -1,6 +1,7 @@
 from app.api.rest.context import RequestContext
 from app.api.rest.gateway import forward_request
 from app.models.accounts import Account
+from app.models.accounts import AccountUpdate
 from app.models.accounts import SignupForm
 from fastapi import APIRouter
 from fastapi import Depends
@@ -17,4 +18,23 @@ async def sign_up(args: SignupForm, ctx: RequestContext = Depends()):
                                      method="POST",
                                      url=f"{SERVICE_URL}/v1/accounts",
                                      json=args.dict())
+    return response
+
+
+# https://osuakatsuki.atlassian.net/browse/V2-58
+@router.get("/v1/accounts/{account_id}", response_model=Account)
+async def get_account(account_id: int, ctx: RequestContext = Depends()):
+    response = await forward_request(ctx,
+                                     method="GET",
+                                     url=f"{SERVICE_URL}/v1/accounts/{account_id}")
+    return response
+
+
+# https://osuakatsuki.atlassian.net/browse/V2-59
+@router.patch("/v1/accounts/{account_id}", response_model=Account)
+async def partial_update_account(account_id: int, args: AccountUpdate,
+                                 ctx: RequestContext = Depends()):
+    response = await forward_request(ctx,
+                                     method="PATCH",
+                                     url=f"{SERVICE_URL}/v1/accounts/{account_id}")
     return response
