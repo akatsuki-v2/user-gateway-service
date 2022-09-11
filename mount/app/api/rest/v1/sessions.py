bc10f6ad3a574ef75ae83ee121e4c272389cfb0f
cmyui <cmyuiosu@gmail.com>
2022-09-11 18:51:44 -0400

    feat: gateway api endpoints for sessions

---
+++ mount/app/api/rest/v1/sessions.py
@@ -0,0 +1,40 @@
+from uuid import UUID
+
+from app.api.rest.context import RequestContext
+from app.api.rest.gateway import forward_request
+from app.models.sessions import LoginForm
+from app.models.sessions import Session
+from fastapi import APIRouter
+from fastapi import Depends
+
+router = APIRouter()
+
+SERVICE_URL = "http://user-accounts-service"
+
+
+# https://osuakatsuki.atlassian.net/browse/V2-11
+@router.post("/v1/sessions", response_model=Session)
+async def log_in(args: LoginForm, ctx: RequestContext = Depends()):
+    response = await forward_request(ctx,
+                                     method="POST",
+                                     url=f"{SERVICE_URL}/v1/sessions",
+                                     json=args.dict())
+    return response
+
+
+# https://osuakatsuki.atlassian.net/browse/V2-12
+@router.patch("/v1/sessions/{session_id}")
+async def partial_update_session(session_id: UUID, ctx: RequestContext = Depends()):
+    response = await forward_request(ctx,
+                                     method="PATCH",
+                                     url=f"{SERVICE_URL}/v1/sessions/{session_id}")
+    return response
+
+
+# https://osuakatsuki.atlassian.net/browse/V2-13
+@router.delete("/v1/sessions/{session_id}")
+async def log_out(session_id: UUID, ctx: RequestContext = Depends()):
+    response = await forward_request(ctx,
+                                     method="DELETE",
+                                     url=f"{SERVICE_URL}/v1/sessions/{session_id}")
+    return response
---
+++ mount/app/models/sessions.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from datetime import datetime
+from uuid import UUID
+
+from . import BaseModel
+
+
+class LoginForm(BaseModel):
+    username: str
+    password: str
+    user_agent: str  # TODO: literal?
+
+
+class Session(BaseModel):
+    session_id: UUID
+    account_id: int
+    user_agent: str
+    expires_at: datetime
+    created_at: datetime
+    updated_at: datetime
+
+# class SessionUpdate(BaseModel):
+#     expires_at: datetime | None
