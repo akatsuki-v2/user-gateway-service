2079089c53a9cc7f82d903bfd0e20c9d0f6d4ed4
cmyui <cmyuiosu@gmail.com>
2022-10-01 02:53:39 -0400

    feat: add chat members api

--- mount/app/api/rest/v1/__init__.py
+++ mount/app/api/rest/v1/__init__.py
@@ -3,11 +3,13 @@ from __future__ import annotations
 from fastapi import APIRouter
 
 from . import accounts
+from . import chat_members
 from . import chats
 from . import sessions
 
 router = APIRouter()
 
 router.include_router(accounts.router, tags=["accounts"])
+router.include_router(chat_members.router, tags=["chat_members"])
 router.include_router(chats.router, tags=["chats"])
 router.include_router(sessions.router, tags=["sessions"])
---
+++ mount/app/api/rest/v1/chat_members.py
@@ -0,0 +1,47 @@
+from app.api.rest.context import RequestContext
+from app.api.rest.gateway import authentication
+from app.api.rest.gateway import forward_request
+from app.models.chat_members import Member
+from app.models.chat_members import MemberInput
+from app.models.sessions import Session
+from fastapi import APIRouter
+from fastapi import Depends
+
+router = APIRouter()
+
+SERVICE_URL = "http://chat-service"
+
+
+# https://osuakatsuki.atlassian.net/browse/V2-86
+@router.post("/v1/chats/{chat_id}/members", response_model=Member)
+async def join_chat(chat_id: int, args: MemberInput,
+                    session: Session = Depends(authentication),
+                    ctx: RequestContext = Depends()):
+    response = await forward_request(ctx,
+                                     method="POST",
+                                     url=f"{SERVICE_URL}/v1/chats/{chat_id}/members",
+                                     json=args.dict())
+    return response
+
+
+# https://osuakatsuki.atlassian.net/browse/V2-87
+@router.delete("/v1/chats/{chat_id}/members", response_model=Member)
+async def leave_chat(chat_id: int,
+                     session: Session = Depends(authentication),
+                     ctx: RequestContext = Depends()):
+    response = await forward_request(ctx,
+                                     method="DELETE",
+                                     url=f"{SERVICE_URL}/v1/chats/{chat_id}/members")
+    return response
+
+
+# https://osuakatsuki.atlassian.net/browse/V2-88
+@router.get("/v1/chats/{chat_id}/members", response_model=list[Member])
+async def get_chat_members(chat_id: int,
+                           # TODO: should this not be authenticated?
+                           session: Session = Depends(authentication),
+                           ctx: RequestContext = Depends()):
+    response = await forward_request(ctx,
+                                     method="GET",
+                                     url=f"{SERVICE_URL}/v1/chats/{chat_id}/members")
+    return response
