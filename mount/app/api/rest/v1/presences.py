7c22633346b83500d57234dace7097083679be45
cmyui <cmyuiosu@gmail.com>
2022-10-07 18:50:58 -0400

    refactor: authentication (2/2)

--- mount/app/api/rest/v1/presences.py
+++ mount/app/api/rest/v1/presences.py
@@ -1,21 +1,26 @@
 from app.api.rest.context import RequestContext
-from app.api.rest.gateway import authentication
+from app.api.rest.gateway import authenticate
 from app.api.rest.gateway import forward_request
 from app.api.rest.responses import Success
 from app.models.presences import Presence
-from app.models.sessions import Session
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi.security import HTTPAuthorizationCredentials as HTTPCredentials
+from fastapi.security import HTTPBearer
 
 router = APIRouter()
 
 SERVICE_URL = "http://users-service"
 
+oauth2_scheme = HTTPBearer()
+
 
 # https://osuakatsuki.atlassian.net/browse/V2-116
 @router.get("/v1/presences/self", response_model=Success[Presence])
-async def get_self_presence(session: Session = Depends(authentication),
+async def get_self_presence(token: HTTPCredentials = Depends(oauth2_scheme),
                             ctx: RequestContext = Depends()):
+    session = await authenticate(ctx, token.credentials)
+
     session_id = session.session_id
     response = await forward_request(ctx,
                                      method="GET",
@@ -25,8 +30,10 @@ async def get_self_presence(session: Session = Depends(authentication),
 
 # https://osuakatsuki.atlassian.net/browse/V2-116
 @router.get("/v1/presences", response_model=Success[list[Presence]])
-async def get_presences(session: Session = Depends(authentication),
+async def get_presences(token: HTTPCredentials = Depends(oauth2_scheme),
                         ctx: RequestContext = Depends()):
+    session = await authenticate(ctx, token.credentials)
+
     response = await forward_request(ctx,
                                      method="GET",
                                      url=f"{SERVICE_URL}/v1/presences")
