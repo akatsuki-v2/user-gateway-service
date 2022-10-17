f6993cf1b2f0117017a1ab26da97875ed4e7cee0
cmyui <cmyuiosu@gmail.com>
2022-10-17 01:18:41 -0400

    feat: add basic forwarding for get scores handler

--- mount/app/api/rest/osu/web.py
+++ mount/app/api/rest/osu/web.py
@@ -1,7 +1,14 @@
+from app.api.rest.context import RequestContext
+from app.api.rest.gateway import forward_request
 from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import Query
 from fastapi import Request
 
 router = APIRouter()
+
+
+SERVICE_URL = "http://bancho-service"
 
 
 # @router.get("/web/{rest_of_path:path}")
@@ -10,3 +17,18 @@ router = APIRouter()
 #     print("Caught ", rest_of_path)
 #     print(await request.body())
 #     return ""
+
+
+@router.get("/web/osu-osz2-getscores.php")
+async def osu_osz2_getscores(request: Request,
+                             username: str = Query(..., alias="us"),
+                             password: str = Query(..., alias="ha"),
+                             ctx: RequestContext = Depends()):
+
+    response = await forward_request(ctx,
+                                     method="GET",
+                                     url=f"{SERVICE_URL}/v1/web/osu-osz2-getscores.php",
+                                     content=await request.body(),
+                                     params=request.query_params)
+
+    return response
